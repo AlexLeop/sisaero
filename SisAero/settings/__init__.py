a50527bef4de8bbfcdf14c2e919276e0158d5966
Alex_leop <lx.leopoldo@outlook.com>
2025-01-03 14:58:49 -0300

    Refatorando a modularização do settings

--- SisAero/settings/__init__.py
+++ SisAero/settings/__init__.py
@@ -1,5 +1,5 @@
 import os
 
 # Define o módulo de configuração padrão com base no ambiente
-settings_module = os.getenv('DJANGO_SETTINGS_MODULE', 'seu_projeto.settings.development')
+settings_module = os.getenv('DJANGO_SETTINGS_MODULE', 'SisAero.settings.development')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
