eea74791ec187bf9e0ebe68f5f04f8505b05ad96
Alex_leop <lx.leopoldo@outlook.com>
2025-01-06 12:38:19 -0300

    Refatorando aero/production.py

--- SisAero/settings/base.py
+++ SisAero/settings/base.py
@@ -8,7 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Configurações básicas
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-9dd13.up.railway.app', 'localhost']
+
 
 # Aplicativos instalados
 INSTALLED_APPS = [
